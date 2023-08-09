6c0e0da1a33c1a1ec37573eb98c4427a6eadd4e7
mmeta007 <138635692+mmeta007@users.noreply.github.com>
2023-08-09 14:55:29 +0800

    Update intZsh.sh

--- intZsh.sh
+++ intZsh.sh
@@ -1,4 +1,6 @@
+#!/bin/sh
 yum install -y curl git zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+#sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/mmeta007/shell/main/zsh.sh)"
 sed -i 's/ZSH_THEME="robbyrussell"/ZSH_THEME="candy"/g' /root/.zshrc
 source /root/.zshrc
