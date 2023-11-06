4f3cb5fca761f64191e1ef780c2561ec21de0725
mmeta007 <138635692+mmeta007@users.noreply.github.com>
2023-11-06 12:40:52 +0800

    Update isUpOrDown.sh

--- isUpOrDown.sh
+++ isUpOrDown.sh
@@ -1,7 +1,7 @@
 appPort=$1
-ip=$(ip a|grep inet|grep -v 127.0.0.1|egrep -v "inet6|br-|docker"|awk '{print $2}'|awk -F '/' '{print $1}')
+#ip=$(ip a|grep inet|grep -v 127.0.0.1|egrep -v "inet6|br-|docker"|awk '{print $2}'|awk -F '/' '{print $1}')
 while true;do
-  nc  -vz ${ip} ${appPort}
+  nc  -vz 127.0.0.1 ${appPort}
   if [ $? -eq 0 ];then
         echo "successed"
         break
